98ae9d1c0bdf0234cd956d49bbbebffa2d1f17b6
Emmagx <emmagx.dev@gmail.com>
2023-02-24 14:08:51 -0600

    ATM comments

--- Projects/ATM/ATM.py
+++ Projects/ATM/ATM.py
@@ -1,13 +1,8 @@
-#####Ejercicio 8########
-#Crear un programa que simule un cajero automatico con un saldo inicial de Q0, con el siguiente mennú
-#1. ingresar dinero
-#2. Retirar Dinero  
-#3. Mostrar Dinero
-#4. Salir
-import time
-saldo=0
+###Proyecto 1, Crear un simulador de cajero automatico, aumentando y subiendo saldos.
+import time #Esta libreria me va ayudar a darle un Delay a los mensajes
+saldo=0 #Saldo inicial, con una base de datos futura, se busca mejorarla.
 print(f"Bienvenido al Cajero")
-while True:
+while True: #Mientras se verdadero
     print("Qué desea realizar?")
     print("1. Ingresar Dinero")
     print("2. Retirar Dinero")
@@ -16,11 +11,11 @@ while True:
     peticion=float(input("Ingrese Numero de petición: "))    
     if peticion==1:
         cantidad=float(input("Inserte cantidad a Ingresar: "))
-        if cantidad>0:
+        if cantidad>0: ##limita que el valor ingresado sea menor a cero.
             saldo+=cantidad
             print(f"Usted ha ingresado {cantidad:.2f} a su cuenta.")
             print(f"Su nuevo saldo es: {saldo:.2f}")
-            time.sleep(2)
+            time.sleep(2) #delay dado, en segundos
         else:
             print(f"La cantidad{cantidad:.2f} es menor a 0. Realice nuevamente la operacion.")            
             time.sleep(2)
@@ -47,7 +42,7 @@ while True:
         if confirmacion=="s":
             print("Gracias por usar nuestro ATM")
             time.sleep(2)
-            break
+            break #ROMPE el bucle si llega a esa opcion.
     else:
         print("Opcion incorrecta")   
         time.sleep(2)
