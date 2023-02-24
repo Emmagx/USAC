c331b095ddb61c09f6f851ac66044f190333f6e8
Emmagx <emmagx.dev@gmail.com>
2023-02-24 13:07:31 -0600

    Actualizar todo

--- Projects/ATM/ATM.py
+++ Projects/ATM/ATM.py
@@ -1,36 +1,53 @@
 #####Ejercicio 8########
-#Crear un programa que simule un cajero automatico con un saldo inicial de Q2000, con el siguiente mennú
+#Crear un programa que simule un cajero automatico con un saldo inicial de Q0, con el siguiente mennú
 #1. ingresar dinero
 #2. Retirar Dinero  
 #3. Mostrar Dinero
 #4. Salir
-loop = 0
+import time
 saldo=0
 print(f"Bienvenido al Cajero")
-while loop==0:
-    peticion=int(input("Que desea realizar? 1. ingresar Dinero; 2. Retirar Dinero. 3. Mostrar Dinero. 4. Salir "))
+while True:
+    print("Qué desea realizar?")
+    print("1. Ingresar Dinero")
+    print("2. Retirar Dinero")
+    print("3. Mostrar Saldo")
+    print("4. Salir")
+    peticion=float(input("Ingrese Numero de petición: "))    
     if peticion==1:
-        SalAum=float(input("Inserte cantidad a Ingresar: "))
-        saldo+=SalAum
-        print(f"Su nuevo Saldo es: {saldo}")
+        cantidad=float(input("Inserte cantidad a Ingresar: "))
+        if cantidad>0:
+            saldo+=cantidad
+            print(f"Usted ha ingresado {cantidad:.2f} a su cuenta.")
+            print(f"Su nuevo saldo es: {saldo:.2f}")
+            time.sleep(2)
+        else:
+            print(f"La cantidad{cantidad:.2f} es menor a 0. Realice nuevamente la operacion.")            
+            time.sleep(2)
     elif peticion==2:
-        SalRes=float(input("Ingrese la cantidad a Retirar: "))
-        if SalRes<=saldo:
-            saldo-=SalRes
-            print(f"Su nuevo saldo es: {saldo}")
+        cantidad=float(input("Ingrese la cantidad a Retirar: "))
+        if cantidad>1:
+            if cantidad<=saldo:
+                saldo-=cantidad
+                print(f"Usted Retiró {cantidad:.2f}")
+                print(f"Su nuevo saldo es: {saldo:.2f}")
+                time.sleep(2)
+            else:
+                print("su saldo es insuficiente")
+                time.sleep(2)
         else: 
-            print("no cuenta con suficiente saldo")
+            print("La cantidad a retirar debe ser un número positivo")
+            time.sleep(2)
     elif peticion==3:
-        print(f"Su saldo es: {saldo}")
+        print(f"Su saldo es: {saldo:.2f}")
+        time.sleep(2)
     elif peticion==4:
         print("Usted esta saliendo del cajero")
-        salir=int(input("Seguro que quiere salir? 1. CONFIRMAR "))
-        if salir==1:
-            loop,salir=salir,loop
-        else:
-            print("Seguiremos en las transacciones")
+        confirmacion=input("Seguro que quiere salir? S/N ").lower()
+        if confirmacion=="s":
+            print("Gracias por usar nuestro ATM")
+            time.sleep(2)
+            break
     else:
-        print("Opcion incorrecta")
-else:
-    print("Gracias por usar el cajero")
-###exit    
+        print("Opcion incorrecta")   
+        time.sleep(2)
