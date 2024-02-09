3b08c04a655909c85b2fe99e16834eac98f13113
Emmagx <emmagx.dev@gmail.com>
2024-02-09 11:28:25 -0600

    Java projects god

---
+++ Java/PRACTICA1/src/practica1/main.java
@@ -0,0 +1,15 @@
+package practica1;
+
+/**
+ *
+ * @author Emmagx
+ * @carnet 202300848
+ * @Nombre Brayan Emanuel Garcia
+ * 
+ */
+public class main {
+
+    public static void main(String[] args) {
+    }
+    
+}
