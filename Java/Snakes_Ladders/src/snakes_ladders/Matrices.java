d9db86f652660476ff6b0850efd61efb58ddefd1
Emmagx <emmagx.dev@gmail.com>
2024-02-11 17:31:04 -0600

    Cosenos y mejoras matrices
    Actualizacion Ley de cosenos Y matrices

--- Java/Snakes_Ladders/src/snakes_ladders/Matrices.java
+++ Java/Snakes_Ladders/src/snakes_ladders/Matrices.java
@@ -9,7 +9,7 @@ package snakes_ladders;
 public class Matrices {
     static Matrices matrices = new Matrices();
 //Declare all the arrays to use
-    static int [][] matriz1Div(){ int [][] matriz1Div = {{5, 10, 1, 3},
+    static double [][] matriz1Div(){ double [][] matriz1Div = {{5, 10, 1, 3},
                                                         {9, 14, 2, 6},
                                                         {7, 8, 15, 3},
                                                         {6, 8, 9, 2},};
@@ -17,25 +17,25 @@ public class Matrices {
                             }
     
     
-    static int [][] matriz2Div(){ int [][]matriz2Div = {{5, 13, 9, 4},
+    static double [][] matriz2Div(){ double [][]matriz2Div = {{5, 13, 9, 4},
                                                         {1, 9, 6, 3},
                                                         {8, 11, 69, 33},
                                                         {25, 6, 7, 4},};
         return matriz2Div;                     
     }
-    static int [][] matriz3Div(){int [][] matriz3Div = {{1, 12, 9, 8},
+    static double [][] matriz3Div(){double [][] matriz3Div = {{1, 12, 9, 8},
                                                         {7, 6, 3, 2},
                                                         {0, 5, 6, 14},
                                                         {6, 9, 6, 10},};
         return matriz3Div;}
     
-    static int [][] matriz4Div(){ int [][] matriz4Div = {{9, 19, 20, 4},
+    static double [][] matriz4Div(){ double [][] matriz4Div = {{9, 19, 20, 4},
                                                          {12, 33, 6, 8},
                                                          {4, 5, 9, 7},
                                                          {8, 22, 14, 6},};
         return matriz4Div;}
     
-    static int [][] matriz1Suma(){ int [][] matriz1Suma = { {7, 48, 5, 0, 1},
+    static int [][] matriz1Suma(){ int [][] matriz1Suma =  {{7, 48, 5, 0, 1},
                                                             {57, 8, 4, 6, 14},
                                                             {0, 5, 6, 78, 15},
                                                             {21, 14, 8, 19, 54},
@@ -71,14 +71,17 @@ public class Matrices {
         return matriz5Suma;}
     
     static int [][] matriz6Suma(){ int [][] matriz6Suma = { {78, 25, 66, 48, 98},
-                            {0, 45, 2, 3, 1},
-                            {2, 9, 14, 10, 20},
-                            {35, 87, 65, 2, 32},
-                            {25, 8, 4, 9, 39}};
+                                                            {0, 45, 2, 3, 1},
+                                                            {2, 9, 14, 10, 20},
+                                                            {35, 87, 65, 2, 32},
+                                                            {25, 8, 4, 9, 39}};
         return matriz6Suma;}
     
     static int [][] matrizSuma = new int [5][5];
-    static int [][]matrizImpresa;
+    static int [][] matrizImpresaSuma;
+    static double [][] matrizImpresaDiv;
+    static double [][] matrizDivision;
+    static float [][] matrizInversa;
     public int [][] sumaMatriz(int [][] a,int [][] b){
         for (int i=0; i<a.length; i++){
             for (int j = 0; j<a.length; j++){
@@ -87,7 +90,7 @@ public class Matrices {
     }
         return matrizSuma;
 }
-    public int [][] imprimirMatriz( int a [][], int b[][]){
+    public int [][] imprimirMatrizSuma( int a [][], int b[][]){
         for (int i=0; i < a.length; i++){
             for (int j = 0; j < a[i].length; j++){
                 System.out.print(" [ " + a[i][j]+ " ] ");
@@ -103,11 +106,11 @@ public class Matrices {
             System.out.println();
         }
         System.out.println("=");
-        return matrizImpresa;    
+        return matrizImpresaSuma;    
     }
-    public int [][] resultados(int [][] a, int [][] b){
+    public int [][] resultadosSuma(int [][] a, int [][] b){
     int [][] matriz1 = new int [5][5];
-        imprimirMatriz(a, b);
+        imprimirMatrizSuma(a, b);
         for (int i=0; i <a.length; i++){
             for (int j = 0; j<a[i].length; j++){
                 matriz1 = matrices.sumaMatriz(a, b);
@@ -117,4 +120,95 @@ public class Matrices {
             }
         return matriz1;
         }
+    
+ public double [][] divisionMatriz(double a[][], double b[][]){
+        //la division es la multiplicacion entre la matriz a y su inversa.
+        double [][] c = matrizInversa(b);
+        double[][] matrizDivision = new double[4][4];
+        for(int i = 0;i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                matrizDivision[i][j] = 0;
+                for (int k = 0; k<4; k++){
+                matrizDivision[i][j] += a[i][k] * c[k][j];
+                
+            }
+            } 
+        }
+        return matrizDivision;
     }
+
+    public double [][] resultadoDivision(double [][] a, double [][] b){
+        imprimirMatrizDivision(a,b);
+        for (int i=0; i <a.length; i++){
+            for (int j = 0; j<a[i].length; j++){
+                matrizDivision = matrices.divisionMatriz(a, b);
+                System.out.print(" [ " + matrizDivision[i][j]+ " ] ");
+                }
+            System.out.println("");
+            }
+        return matrizDivision;
+    }
+    public double [][] imprimirMatrizDivision(double [][]a, double b[][]){
+        for (int i=0; i < a.length; i++){
+            for (int j = 0; j < a[i].length; j++){
+                System.out.print(" [ " + a[i][j]+ " ] ");
+                
+            }
+            System.out.println();
+        }
+        System.out.println("/");
+        for (int i=0; i < b.length; i++){
+            for (int j = 0; j < b[i].length; j++){
+                System.out.print(" [ " + b[i][j]+ " ] ");
+            }
+            System.out.println();
+        }
+        System.out.println("=");
+
+        return matrizImpresaDiv;
+    }
+
+    public static double[][] matrizInversa(double[][] a) {
+    double s0 = a[0][0] * a[1][1] - a[1][0] * a[0][1];
+    double s1 = a[0][0] * a[1][2] - a[1][0] * a[0][2];
+    double s2 = a[0][0] * a[1][3] - a[1][0] * a[0][3];
+    double s3 = a[0][1] * a[1][2] - a[1][1] * a[0][2];
+    double s4 = a[0][1] * a[1][3] - a[1][1] * a[0][3];
+    double s5 = a[0][2] * a[1][3] - a[1][2] * a[0][3];
+
+    double c5 = a[2][2] * a[3][3] - a[3][2] * a[2][3];
+    double c4 = a[2][1] * a[3][3] - a[3][1] * a[2][3];
+    double c3 = a[2][1] * a[3][2] - a[3][1] * a[2][2];
+    double c2 = a[2][0] * a[3][3] - a[3][0] * a[2][3];
+    double c1 = a[2][0] * a[3][2] - a[3][0] * a[2][2];
+    double c0 = a[2][0] * a[3][1] - a[3][0] * a[2][1];
+
+    // Se debe verificar si el determinante es cero
+    double invdet = 1.0 / (s0 * c5 - s1 * c4 + s2 * c3 + s3 * c2 - s4 * c1 + s5 * c0);
+
+    double[][] b = new double[4][4];
+
+    b[0][0] = (a[1][1] * c5 - a[1][2] * c4 + a[1][3] * c3) * invdet;
+    b[0][1] = (-a[0][1] * c5 + a[0][2] * c4 - a[0][3] * c3) * invdet;
+    b[0][2] = (a[3][1] * s5 - a[3][2] * s4 + a[3][3] * s3) * invdet;
+    b[0][3] = (-a[2][1] * s5 + a[2][2] * s4 - a[2][3] * s3) * invdet;
+
+    b[1][0] = (-a[1][0] * c5 + a[1][2] * c2 - a[1][3] * c1) * invdet;
+    b[1][1] = (a[0][0] * c5 - a[0][2] * c2 + a[0][3] * c1) * invdet;
+    b[1][2] = (-a[3][0] * s5 + a[3][2] * s2 - a[3][3] * s1) * invdet;
+    b[1][3] = (a[2][0] * s5 - a[2][2] * s2 + a[2][3] * s1) * invdet;
+
+    b[2][0] = (a[1][0] * c4 - a[1][1] * c2 + a[1][3] * c0) * invdet;
+    b[2][1] = (-a[0][0] * c4 + a[0][1] * c2 - a[0][3] * c0) * invdet;
+    b[2][2] = (a[3][0] * s4 - a[3][1] * s2 + a[3][3] * s0) * invdet;
+    b[2][3] = (-a[2][0] * s4 + a[2][1] * s2 - a[2][3] * s0) * invdet;
+
+    b[3][0] = (-a[1][0] * c3 + a[1][1] * c1 - a[1][2] * c0) * invdet;
+    b[3][1] = (a[0][0] * c3 - a[0][1] * c1 + a[0][2] * c0) * invdet;
+    b[3][2] = (-a[3][0] * s3 + a[3][1] * s1 - a[3][2] * s0) * invdet;
+    b[3][3] = (a[2][0] * s3 - a[2][1] * s1 + a[2][2] * s0) * invdet;
+
+    return b;
+}
+
+}
--- Java/Snakes_Ladders/src/snakes_ladders/ProblemaCosenos.java
+++ Java/Snakes_Ladders/src/snakes_ladders/ProblemaCosenos.java
@@ -1,8 +1,62 @@
 package snakes_ladders;
 public class ProblemaCosenos {
-    public int cosenos(){
+    static int resultadosCosenos;
+    //Formula c**2=a**2 + a*b * cos(C)
+    //Datos por triangulo:
+    //Triangulo 1:
+    static double ladoA, respuesta;
+    static double rightAnswer[];
+    static double ladoC;
+    static double ladoB;
+    static double alpha, beta, gama;
+        public double [] case1Coseno(){
+            ladoA = 15; 
+            ladoC = 20; 
+            alpha = 25;
+            //lado B
+            ladoB = Math.sqrt(Math.pow(ladoA, 2) + Math.pow(ladoC, 2) - 2*ladoA*ladoC*Math.cos(alpha));
+            //angulo B
+            beta = Math.acos((Math.pow(ladoB, 2) - Math.pow(ladoA, 2) - Math.pow(ladoC, 2))/ (2*ladoA*ladoB));
+            //angulo C
+            gama = Math.acos((Math.pow(ladoC, 2) - Math.pow(ladoA, 2) - Math.pow(ladoB, 2))/ (2*ladoA*ladoC));
+            rightAnswer[0] = ladoB;
+            rightAnswer[1] = Math.toDegrees(beta);
+            rightAnswer[2] = Math.toDegrees(gama);
+            return rightAnswer;
+    }
+        public double[] case2Coseno(){
+            ladoB = 10; 
+            ladoC = 25; 
+            beta = 30;
+            //lado A
+            ladoA = Math.sqrt(Math.pow(ladoB, 2) + Math.pow(ladoC, 2) - 2 * ladoB * ladoC * Math.cos(beta));
+            //angulo alpha
+            alpha = Math.acos((Math.pow(ladoA, 2) + Math.pow(ladoC, 2) - Math.pow(ladoB, 2)) / (2 * ladoA * ladoC));
+            //angulo C
+            gama = Math.acos((Math.pow(ladoB, 2) + Math.pow(ladoA, 2) - Math.pow(ladoC, 2)) / (2 * ladoB * ladoA));
+            rightAnswer[0] = ladoA;
+            rightAnswer[1] = Math.toDegrees(alpha);
+            rightAnswer[2] = Math.toDegrees(gama);
+            return rightAnswer;        
+    }
+        public double [] case3Coseno(){
+            ladoA = 18; 
+            ladoB = 25; 
+            gama = 30;
+            //lado B
+            ladoC = Math.sqrt(Math.pow(ladoA, 2) + Math.pow(ladoB, 2) - 2 * ladoA * ladoB * Math.cos(gama));
+            //angulo B
+            alpha = Math.acos((Math.pow(ladoB, 2) + Math.pow(ladoC, 2) - Math.pow(ladoA, 2)) / (2 * ladoB * ladoC));
+            //angulo C
+            beta = Math.acos((Math.pow(ladoC, 2) + Math.pow(ladoA, 2) - Math.pow(ladoB, 2)) / (2 * ladoC * ladoA));
+            rightAnswer[0] = ladoC;
+            rightAnswer[1] = Math.toDegrees(alpha);
+            rightAnswer[2] = Math.toDegrees(beta);
+            return rightAnswer;
+    }
+    public int resultadosCosenos(){
     
-        return 0;
+        return resultadosCosenos;
     
     }
     
